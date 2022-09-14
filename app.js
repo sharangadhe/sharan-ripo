9f8958eb1a1e053f4daa8b876285c1646063262c
gadhesharan <sharangadhe32@gmail.com>
2022-09-15 01:18:07 +0530

    local storage with api

---
+++ app.js
@@ -0,0 +1,34 @@
+// const arr =[1,2,3]
+
+// const obj = {
+//     name:"C class",
+//     brand:"Mercedes",
+//     model:"2022",
+//     log:function(){
+//         console.log(this)
+//     }
+// }
+// obj.log()
+// console.log(window)
+// const name = window.prompt('what is your name')
+// console.log(name)
+// const age= window.prompt('what is your age')
+// console.log(age)
+
+// console.log('h1')
+// function great(){
+//     console.log("hi")
+// }
+// great()
+
+
+
+//web api
+// window.alert('hi')
+// console.log(window.innerHeight)
+
+// localStorage.setItem('brand',"Mercedes")
+// localStorage.setItem('Model',"C Class")
+
+// console.log(localStorage.getItem('brand'))
+localStorage.removeItem('brand')
